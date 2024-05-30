d26c7890d9e54ea2666848d0b551e68c3f27422b
pythonhack76 <lrulvoni@gmail.com>
2024-05-30 17:36:05 +0200

    update images

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 
 def posts_list(request):
@@ -8,7 +8,9 @@ def posts_list(request):
     return render(request, 'posts/posts_list.html', { 'posts': posts})
 
 def post_page(request, slug):
-    return HttpResponse(slug)
+    #return HttpResponse(slug)
+    posts = Post.objects.get(slug=slug) 
+    return render(request, 'posts/post_page.html', { 'post': posts})
 
 
 
